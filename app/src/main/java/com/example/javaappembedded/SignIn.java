39481c55a03523e9a2ea957a344771e94501304a
jjowoo <1201wogus@naver.com>
2022-09-17 23:57:19 +0900

    9/17

---
+++ app/src/main/java/com/example/javaappembedded/SignIn.java
@@ -0,0 +1,22 @@
+package com.example.javaappembedded;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class SignIn extends AppCompatActivity {
+
+    EditText phone_num;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+
+
+
+
+    }
+}
