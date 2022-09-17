f99a5006685a15480e56d6f48113d1cf99b5fffc
JJowoo <34655168+JJowoo@users.noreply.github.com>
2022-09-17 14:32:07 +0900

    9/16

--- app/src/main/java/com/example/javaappembedded/Mainpage.java
+++ app/src/main/java/com/example/javaappembedded/Mainpage.java
@@ -2,13 +2,27 @@ package com.example.javaappembedded;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Mainpage extends AppCompatActivity {
+
+    TextView login_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+
+        login_result=findViewById(R.id.login_result);
+
+        Intent intent=getIntent();
+
+        Bundle bundle=intent.getExtras();
+        String email=bundle.getString("email");
+        String password = bundle.getString("password");
+
+        login_result.setText(email+"//"+password);
     }
 }
--- app/src/main/java/com/example/javaappembedded/login.java
+++ app/src/main/java/com/example/javaappembedded/login.java
@@ -2,7 +2,10 @@ package com.example.javaappembedded;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -10,6 +13,7 @@ import com.google.android.material.textfield.TextInputEditText;
 public class login extends AppCompatActivity {
 
     EditText editText_email,editText_Password;
+    Button button_login;
 
 
     @Override
@@ -19,6 +23,22 @@ public class login extends AppCompatActivity {
 
         editText_email=findViewById(R.id.editText_email);
         editText_Password=findViewById(R.id.editText_Password);
+        button_login=findViewById(R.id.button_login);
+
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=editText_email.getText().toString();
+                String password= editText_Password.getText().toString();
+
+                Intent intent=new Intent(login.this, Mainpage.class);
+                intent.putExtra("email",email);
+                intent.putExtra("password",password);
+                startActivity(intent);
+            }
+        });
+
+
 
 
     }
