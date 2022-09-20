33634bf622158ab1eb654a92fb0d9384ede4504f
SeokYoon99 <csysyjy@gmail.com>
2022-09-20 17:54:37 +0900

    버튼으로 화면간 전환 설정 로그인 창에서 textview의 signin을 signup의 버튼으로 만들어야 함 signin액티비티를 signup으로 이름변경 필요(signin=로그인, signup=회원가입)

--- app/src/main/java/com/example/javaappembedded/SheetAdjust.java
+++ app/src/main/java/com/example/javaappembedded/SheetAdjust.java
@@ -3,6 +3,11 @@ package com.example.javaappembedded;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class SheetAdjust extends AppCompatActivity {
 
@@ -10,5 +15,29 @@ public class SheetAdjust extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sheet_adjust);
+
+        Button button_carpay = findViewById(R.id.button_carpay);
+        Button button_home = findViewById(R.id.button_home);
+        Button button_more = findViewById(R.id.button_more);
+
+
+        button_carpay.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),Mainpage.class);
+                startActivity(intent);
+            }
+        });
+        button_home.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),Home.class);
+                startActivity(intent);
+            }
+        });
+        button_more.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),SettingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
