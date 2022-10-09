ef4209726a288e5547836a9916dcef0c3f1966f8
jjowoo <1201wogus@naver.com>
2022-10-09 20:32:08 +0900

    합쳐야함

--- app/src/main/java/com/example/javaappembedded/Home.java
+++ app/src/main/java/com/example/javaappembedded/Home.java
@@ -1,5 +1,6 @@
 package com.example.javaappembedded;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,8 +8,18 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class Home extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +30,40 @@ public class Home extends AppCompatActivity {
         Button button_position = findViewById(R.id.button_position);
         Button button_more = findViewById(R.id.button_more);
         Button button_createkey = findViewById(R.id.create_key);
+        Button unlock = findViewById(R.id.unlock);
+
         //intent안의 uid값을 받아옴
         Intent intent = getIntent();
         String uid = intent.getStringExtra("uid");
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        final String[] unlock_status = {""};
+
+        mDatabase.child("unlock").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                unlock_status[0] = snapshot.getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        unlock.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                if(unlock_status[0].equals("0")){
+                    mDatabase.child("unlock").setValue("1");
+                }
+                else{
+                    mDatabase.child("unlock").setValue("0");
+                }
+
+            }
+        });
 
 
         button_carpay.setOnClickListener(new View.OnClickListener(){
